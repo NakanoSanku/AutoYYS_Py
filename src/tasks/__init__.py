118d3a5523040e0206526412fd15d3a8b8d77c10
KateTseng <alexkaykt@outlook.com>
2024-05-07 19:27:47 +0800

    导入抽厕纸和场景切换功能

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -5,3 +5,5 @@ from .explore import Explore  # 探索任务
 from .fightSkill import FightSkill
 from .secretStory import SecretStory
 from .personalBreakthrough import PersonalBreakthrough
+from .toiletPaper import ToiletPaper
+from .sceneChange import EnterScene
